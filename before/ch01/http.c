92263896c857a73ae7bb3c87b3b10e3fa6d8615c
Otto Giron <ottog2486@gmail.com>
2017-02-09 00:33:12 -0600

    Simple http.c implementation

--- before/ch01/http.c
+++ before/ch01/http.c
@@ -13,3 +13,71 @@
 #include <unistd.h>
 #endif
 #include "base64.h"
+
+#define HTTP_PORT 80
+
+/**
+ * Accept a well-formed URL (e.g. http://www.company.com/index.html) and return
+ * pointers to the host part and the path part. Note that this function
+ * modifies the uri itself as well. It returns 0 on success, −1 if the URL is
+ * found to be malformed in any way.
+ */
+
+int parse_url(char *uri, char **host, char **path)
+{
+    char *pos;
+
+    pos = strstr(uri, "//");
+
+    if (!pos)
+    {
+        return -1;
+    }
+
+    *host = pos + 2;
+
+    pos = strchr(*host, '/');
+
+    if (!pos)
+    {
+        *path = NULL;
+    }
+    else
+    {
+        *pos = '\0';
+        *path = pos + 1;
+    }
+
+    return 0;
+}
+
+/**
+    * Simple command-line HTTP client.
+ */
+
+int main(int argc, char *argv[])
+{
+    int client_connection;
+    char *host, *path;
+
+    struct hostent *host_name;
+
+    struct sockaddr host_address;
+
+#ifdef WIN32
+    WSADATA wsaData;
+#endif
+
+    if (argc < 2)
+    {
+        fprintf(stderr, "Usage:%s <URL>\n", argv[0]);
+        return 1;
+    }
+
+    if (parse_url(argv[1], &host, &path))
+    {
+        fprintf(stderr, "Error - malformed URL '%s'\n", argv[1]);
+    }
+
+    printf("Connecting to host %s\n", host);
+}
