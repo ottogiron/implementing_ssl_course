f7415deed4cdd77499588160292fac200af43d34
Otto Giron <ottog2486@gmail.com>
2017-02-11 00:02:40 -0600

    Implement simple http client

--- before/ch01/http.c
+++ before/ch01/http.c
@@ -51,9 +51,68 @@ int parse_url(char *uri, char **host, char **path)
     return 0;
 }
 
+
+
+
+
+#define MAX_GET_COMMAND 255
+/**
+ * Format and send an HTTP get command. The return value will be 0
+ * on success, −1 on failure, with errno set appropriately. The caller
+ * must then retrieve the response.
+ */
+
 /**
     * Simple command-line HTTP client.
  */
+
+int  http_get(int connection, const char *path, const char *host)
+{
+    static char get_command[MAX_GET_COMMAND];
+
+    sprintf( get_command, "GET /%s HTTP/1.1\r\n", path );
+
+    if (send(connection, get_command, strlen(get_command), 0) == 1)
+    {
+        return -1;
+    }
+
+    sprintf( get_command, "Host: %s\r\n", host );
+    if (send(connection, get_command, strlen(get_command), 0) == -1)
+    {
+        return -1;
+    }
+
+    sprintf( get_command, "Connection: close\r\n\r\n" );
+
+    if (send(connection, get_command, strlen(get_command), 0) == -1)
+    {
+        return -1;
+    }
+
+    return 0;
+}
+
+
+#define BUFFER_SIZE 255
+
+/**
+ * Receive all data available on a connection and dump it to stdout
+ */
+void display_result(int connection)
+{
+    int received = 0; 
+    static char recv_buf[BUFFER_SIZE + 1];
+
+    while((received = recv(connection, recv_buf, BUFFER_SIZE,0))> 0)
+    {
+        recv_buf[received] = '\0';
+        printf("%s", recv_buf);
+    }
+
+    printf("\n");
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -62,7 +121,7 @@ int main(int argc, char *argv[])
 
     struct hostent *host_name;
 
-    struct sockaddr host_address;
+    struct sockaddr_in host_address;
 
 #ifdef WIN32
     WSADATA wsaData;
@@ -80,4 +139,63 @@ int main(int argc, char *argv[])
     }
 
     printf("Connecting to host %s\n", host);
+
+//Step 1: open a socket connection on http port with the destination host.
+
+#ifdef WIN32
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != NO_ERROR)
+    {
+        fprintf(stderr, "Error, unable to initialize winsock.\n");
+        return 2;
+    }
+#endif
+
+    client_connection = socket(PF_INET, SOCK_STREAM, 0);
+
+    if (!client_connection)
+    {
+        perror("Unable to create local socket");
+    }
+
+    host_name = gethostbyname(host);
+
+    if (!host_name)
+    {
+        perror("Error in name resolution");
+    }
+
+    host_address.sin_family = AF_INET;
+    host_address.sin_port = htons(HTTP_PORT);
+    memcpy(&host_address.sin_addr, host_name->h_addr_list[0], sizeof(struct in_addr));
+
+    if (connect(client_connection, (struct sockaddr *)&host_address, sizeof(host_address)) == 1)
+    {
+        perror("Unable to connect to host"); 
+        return 4;
+    }
+
+    printf("Retrieving document: %s\n", path);
+
+    http_get(client_connection, path, host);
+
+    display_result(client_connection);
+
+    printf("Shutting down.\n");
+
+#ifdef WIN32
+    if (closesocket(client_connection) == −1)
+#else
+    if (close(client_connection) == -1)
+#endif
+    {
+        perror("Error closing client connection");
+        return 5;
+    }
+
+#ifdef WIN32
+    WSACleanup();
+#endif
+
+    return 0;
+    
 }
